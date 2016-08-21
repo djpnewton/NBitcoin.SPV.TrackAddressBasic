acc124e935d2487bedc2018bcd57865a31abb989
Daniel Newton <djpnewton@gmail.com>
2016-08-22 09:53:44 +1200

    rework so we can save our progress and reconnect when TrackerBehaviour disconnects our trusted node for too many false positives

--- NBitcoin.SPV.TrackAddressBasic/Program.cs
+++ NBitcoin.SPV.TrackAddressBasic/Program.cs
@@ -24,63 +24,34 @@ namespace NBitcoin.SPV.TrackAddressBasic
             var ipaddr = IPAddress.Parse("127.0.0.1");
             var peer = new NetworkAddress(ipaddr, 18333);
 
-            var parameters = new NodeConnectionParameters();
-
-            // ping pong
-            parameters.TemplateBehaviors.FindOrCreate<PingPongBehavior>();
-
-            // chain behavior keep chain in sync
-            var chain = new ConcurrentChain(network);
-            parameters.TemplateBehaviors.Add(new ChainBehavior(chain));
-
-            // tracker behavior tracks our addresses
-            var tracker = new Tracker();
-            var trackerBehavior = new TrackerBehavior(tracker, chain);
-            parameters.TemplateBehaviors.Add(trackerBehavior);
-
             // initial scan location
             var scanLocation = new BlockLocator();
             scanLocation.Blocks.Add(network.GetGenesis().GetHash());
 
             // connect node 
-            var node = Node.Connect(network, peer, parameters);
-            trackerBehavior.Attach(node); // TODO: not sure why we need to do this manually
-
-            // debug fluff
-            node.MessageReceived += (node1, message) =>
+            var chain = new ConcurrentChain(network);
+            Node node = null;
+            NodeEventHandler nodeDisconnect = null;
+            nodeDisconnect = (Node node1) =>
             {
-                if (message.Message.Command != "headers" && message.Message.Command != "merkleblock")
-                    Console.WriteLine(message.Message.Command);
-            };
-
-            // setup tracker with our addresse
-            node.StateChanged += (node1, oldState) =>
-            {
-                if (node1.State == NodeState.HandShaked)
-                {
-                    trackerBehavior.Scan(scanLocation, DateTimeOffset.MinValue);
-                    trackerBehavior.SendMessageAsync(new MempoolPayload());
-
-                    tracker.Add(key.ScriptPubKey);
-
-                    trackerBehavior.RefreshBloomFilter();
-                }
-            };
-
-            node.Disconnected += (Node node1) =>
-            {
+                // TrackerBehavior has probably disconnected the node because of too many false positives...
                 Console.WriteLine("Disconnected!");
-            };
 
-            // more debug fluff
-            tracker.NewOperation += (Tracker sender, Tracker.IOperation trackerOperation) =>
-            {
-                Console.WriteLine("tracker operation: {0}", trackerOperation.ToString());
-            };
+                // save progress
+                var trackerBehavior = node1.Behaviors.Find<TrackerBehavior>();
+                scanLocation = trackerBehavior.CurrentProgress;
 
-            // do node handshake
-            node.VersionHandshake();
-            Console.WriteLine("Successful handshake with {0}", peer.Endpoint);
+                Task.Run(() =>
+                {
+                    System.Threading.Thread.Sleep(1000);
+
+                    // reconnect
+                    node = ConnectNode(key, peer, chain, scanLocation, nodeDisconnect);
+                    HandshakeNode(node, peer);
+                });
+            };
+            node = ConnectNode(key, peer, chain, scanLocation, nodeDisconnect);
+            HandshakeNode(node, peer);
 
             int chainHeight = 0;
             while (true)
@@ -92,6 +63,69 @@ namespace NBitcoin.SPV.TrackAddressBasic
                 }
                 System.Threading.Thread.Sleep(5000);
             }
+        }
+
+        private static Node ConnectNode(Key key, NetworkAddress peer, ConcurrentChain chain, BlockLocator scanLocation, NodeEventHandler nodeDisconnect)
+        {
+            var parameters = new NodeConnectionParameters();
+
+            // ping pong
+            parameters.TemplateBehaviors.FindOrCreate<PingPongBehavior>();
+
+            // chain behavior keep chain in sync
+            parameters.TemplateBehaviors.Add(new ChainBehavior(chain));
+
+            // tracker behavior tracks our address
+            Console.WriteLine("Tracking {0}", key.PubKey.GetAddress(network));
+            var tracker = new Tracker();
+            tracker.Add(key.ScriptPubKey);
+            var trackerBehavior = new TrackerBehavior(tracker, chain);
+            parameters.TemplateBehaviors.Add(trackerBehavior);
+            tracker.NewOperation += (Tracker sender, Tracker.IOperation trackerOperation) =>
+            {
+                Console.WriteLine("tracker operation: {0}", trackerOperation.ToString());
+            };
+
+            var node = Node.Connect(network, peer, parameters);
+            trackerBehavior.Detach();
+            trackerBehavior.Attach(node);
+
+            // debug fluff
+            node.MessageReceived += (node1, message) =>
+            {
+                if (message.Message.Command != "headers" && message.Message.Command != "merkleblock")
+                {
+                    if (message.Message.Payload is TxPayload)
+                    {
+                        var txPayload = (TxPayload)message.Message.Payload;
+                        Console.WriteLine("tx {0}", txPayload.Object.GetHash());
+                    }
+                        else
+                    Console.WriteLine(message.Message.Command);
+                }
+            };
+
+            // start tracker scanning
+            node.StateChanged += (node1, oldState) =>
+            {
+                if (node1.State == NodeState.HandShaked)
+                {
+                    trackerBehavior.Scan(scanLocation, DateTimeOffset.MinValue);
+                    trackerBehavior.SendMessageAsync(new MempoolPayload());
+
+                    trackerBehavior.RefreshBloomFilter();
+                }
+            };
+
+            node.Disconnected += nodeDisconnect;
+
+            return node;
+        }
+
+        private static void HandshakeNode(Node node, NetworkAddress peer)
+        {
+            node.VersionHandshake();
+            Console.WriteLine("Successful handshake with {0}", peer.Endpoint);
         }
     }
 }
